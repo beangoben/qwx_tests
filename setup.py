8b2f2d15f79cdff8295efaae17046726b073ce9f
Benjamin Sanchez-Lengeling <beangoben@gmail.com>
2017-06-20 01:36:30 -0400

    basic tests

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='qwx_tests',
+      version='0.1',
+      description='QuantumWorldX tests',
+      url='',
+      author='',
+      author_email='',
+      license='MIT',
+      packages=['qwx_tests'],
+      zip_safe=False)
